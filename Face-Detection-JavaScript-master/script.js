139179b3e000a96d5503a94bb0075cfced151b67
xiang1224 <a10083237@gmail.com>
2022-10-10 16:06:07 +0800

    1010

--- Face-Detection-JavaScript-master/script.js
+++ Face-Detection-JavaScript-master/script.js
@@ -1,12 +1,12 @@
 const video = document.getElementById('video')
-
+//載入模型
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
-
+//開啟webcam, 把webcam引入video 顯示
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -14,18 +14,19 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
+//播放
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  document.body.append(canvas)//插入畫布
   const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
+  faceapi.matchDimensions(canvas, displaySize)//設定顯示尺寸
+  setInterval(async () => {//每 100ms 去繪製
+//識別位置, 輪廓, 表情
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)//清空畫布
+    faceapi.draw.drawDetections(canvas, resizedDetections)//臉部探測
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)//輪廓顯示
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)//表情顯示
   }, 100)
 })
--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-
+//連結 Chrome 和 Firefox 上的 SpeechRecognition API
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+//要使用SpeechRecognition，要先建立一個SpeechRecognition物件
 const recognition = new SpeechRecognition();
+//套用物件
 const mic = document.querySelector('#mic');
 const colorful = document.querySelector('#colorful');
+//點擊啟動
 mic.onclick = () => {
   recognition.start();
 }
+//辨識有任何結果時會呼叫這個函數
 recognition.onresult = (event) => {
   const color = event.results[0][0].transcript;
+//print
   console.log(color);
+//連到背景
   colorful.style.backgroundColor = color;
 }
 
